a0c304077de14d04210943b9b21a7139ec473e71
JDC-GG <juan.cordoba0609@gmail.com>
2025-06-03 14:37:44 -0500

    actu

--- app/src/main/java/com/example/faunafinder/MainActivity.kt
+++ app/src/main/java/com/example/faunafinder/MainActivity.kt
@@ -4,13 +4,13 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import com.example.faunafinder.navigation.AppNavigation
-import com.example.faunafinder.ui.theme.FaunaFInderTheme  // Asegúrate de importar tu theme
+import com.example.faunafinder.ui.theme.FaunaFInderTheme
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-            FaunaFInderTheme {  // <-- Aquí aplicas el theme
+            FaunaFInderTheme {
                 AppNavigation()
             }
         }
